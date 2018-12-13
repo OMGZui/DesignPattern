e0f9ae19a7b70eb7122ff8c3298c5d27d1972cde
小粽子 <862275679@qq.com>
2018-12-13 11:15:25 +0800

    zsh

--- dev.sh
+++ dev.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 echo -n "输入你改动的内容："
 read msg
 
-echo -n "---开始构建---"
+echo "---开始构建---\n"
 git add .
 git commit -m "$msg"
 git push origin master
-echo -n "---构建完成---"
+echo "\n---构建完成---"
